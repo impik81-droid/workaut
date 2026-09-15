b35075a28e0914ae321af14e966a50850f8a60b0
impik81-droid <impik81@gmail.com>
2026-09-15 16:44:43 +0300

    Refactor server.js for improved structure and clarity

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-[source: 4]const express = require('express');
+const express = require('express');
 const cors = require('cors');
 const multer = require('multer');
 const axios = require('axios');
